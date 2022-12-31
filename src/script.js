090b65ebac4bffcac50c7ab2a0ce5451a5d1463c
JG3955 <118084215+JG3955@users.noreply.github.com>
2022-12-30 22:23:03 -0600

    created js to populate current day, date, time

---
+++ src/script.js
@@ -0,0 +1,56 @@
+let now = new Date();
+
+let currentDay = document.querySelector("li#day");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+currentDay.innerHTML = day;
+
+function formatDate(date) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let dates = date.getDate();
+  let year = date.getFullYear();
+  return `${month} ${dates}, ${year}`;
+}
+
+let currentMonthDateYear = document.querySelector("li#month-date-year");
+
+currentMonthDateYear.innerHTML = formatDate(now);
+
+function formatTime(time) {
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+let currentTime = document.querySelector("li#time");
+currentTime.innerHTML = formatTime(now);
